9f21e5fef09e5bd1ca3da30af744c532a86abbbe
Thibault <th.delavelle@gmail.com>
2016-02-02 11:53:16 +0100

    On ne dessine plus que les triangles visibles en 2D + correctif de la tolerance du calcul des barycentres pour savoir si un point est à dessiner ou pas

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <vector>
 #include <cstdlib>
+#include <limits>
 using namespace std;
 using std::ifstream;
 
@@ -103,6 +104,7 @@ vector<vector<float> > parsePoints(ifstream &fin, TGAImage &image){
 
 vector<vector<float> > parseTriangles(ifstream &fin, TGAImage &image){
 
+
     vector<vector<float> >nuage;
     vector<float> points;
 
@@ -174,7 +176,7 @@ void setNuageSurImage(vector<vector<float> > nuage, TGAImage &image) {
 	image.write_tga_file("Nuage.tga");
 }
 
-bool isPointDansTriangle(int px, int py, int ax, int ay, int bx, int by, int cx, int cy){
+bool isPointDansTriangle(int px, int py, int ax, int ay, int az,int bx, int by, int bz,int cx, int cy,int cz, int buffer[][500]){
 /*
 On calcule le produit matriciel suivant
    ( (Bx - Ax) (Cx - Ax) (Ax - Px) ) (u)
@@ -231,27 +233,35 @@ float v = A[1][0]*B[0][0] + A[1][1]*B[1][0];
 //printf("V = %f\n",v);
 //printf("1-U-V = %f\n",(1-u-v));
 float toto = 1-u-v;
-if(u<0 || v<0 || toto<0)return false;
-else return true;
 
+//On prend en compte la profondeur pour ne pas tout dessiner
+if(u>=-1e-5 && v>=-1e-5 && toto>=-1e-5){ //on utilise un facteur d'approximation car des fois meme si c'est <0 on doit le prendre
+    float pz = (u*az) + (v*bz) + (toto*cz);
+    if(buffer[px][py]>pz)return false;
+    else buffer[px][py]=pz;
+    return true;
+}
+else return false;
 
 }
 
 
-void setRemplissageTriangleBarycentric(int x1, int y1, int x2, int y2, int x3, int y3,TGAImage &image){
+void setRemplissageTriangleBarycentric(int x1, int y1, int z1, int x2, int y2, int z2,int x3, int y3,int z3,TGAImage &image, int buffer[][500]){
 
     int minX = min(x3,min(x1,x2));
     int minY = min(y3,min(y1,y2));
     int maxX = max(x3,max(x1,x2));
     int maxY = max(y3,max(y1,y2));
 
-    for(int px = minX+1; px < maxX ; px++){
-       for(int py = minY+1;py < maxY; py++){
-           if(isPointDansTriangle(px,py,x1,y1,x2,y2,x3,y3)){
-              image.set(px,py,white);
-                }
+    TGAColor rndcolor = TGAColor(rand()%255, rand()%255, 255, 255);
+    for(int px = minX ; px < maxX ; px++){
+       for(int py = minY ; py < maxY ; py++){
+           if(isPointDansTriangle(px,py,x1,y1,z1,x2,y2,z2,x3,y3,z3,buffer)){
+              image.set(px,py,rndcolor);
+
             }
         }
+    }
 }
 
 void setRemplissageTriangleLineSweep(int x1, int y1, int x2, int y2, int x3, int y3,TGAImage &image){
@@ -275,7 +285,7 @@ void setRemplissageTriangleLineSweep(int x1, int y1, int x2, int y2, int x3, int
     // Dessin de la partie basse (y entre y1 et y2)
     int hauteur = y3 - y1;
     for(int a = y1; a<y2 ; a++){
-        int semiHauteur = y2-y1+1;
+        int semiHauteur = y2-y1;
         float alpha = (float)(a-y1)/hauteur;
         float beta = (float) (a-y1)/semiHauteur;
         int ax = x1 + (x3-x1)*alpha;
@@ -283,11 +293,11 @@ void setRemplissageTriangleLineSweep(int x1, int y1, int x2, int y2, int x3, int
 
         if(ax>bx)swap(ax,bx);
         for(int b = ax;b<bx;b++){
-            image.set(b,a,green);
+            image.set(b,a,white);
         }
     }
     for(int a = y2; a<y3 ; a++){
-        int semiHauteur = y3-y2+1;
+        int semiHauteur = y3-y2;
         float alpha = (float)(a-y1)/hauteur;
         float beta = (float) (a-y2)/semiHauteur;
         int ax = x1 + (x3-x1)*alpha;
@@ -295,34 +305,47 @@ void setRemplissageTriangleLineSweep(int x1, int y1, int x2, int y2, int x3, int
 
         if(ax>bx)swap(ax,bx);
         for(int b = ax;b<bx;b++){
-            image.set(b,a,red);
+            image.set(b,a,white);
         }
     }
 }
 
 void setTrianglesSurImage(vector<vector<float> > nuage, vector<vector<float> > triangles, TGAImage &image) {
     int a,b,c; // les 3 sommets stockés dans "triangles"
-    int x1,y1,x2,y2,x3,y3;
+    int x1,y1,x2,y2,x3,y3,z1,z2,z3;
+    //on remplit le buffer avec +infini dans chaque case
+    int buffer[500][500];
+    for(int i=0;i<500;i++){
+        for(int j=0;j<500;j++){
+            buffer[i][j] = numeric_limits<int>::min();
+        }
+    }
 
 	for (unsigned int i = 0; i < triangles.size(); ++i) {
         a = triangles[i][0]-1; //on enleve 1 pour atteindre la bonne ligne dans le vector de points
         b = triangles[i][1]-1;
         c = triangles[i][2]-1;
 
-        x1 = nuage[a][0]+image.get_width()/2;
-        y1 = nuage[a][1]+image.get_height()/2;
+        x1 = nuage[a][0]+image.get_width()/2. + .5 ;
+        y1 = nuage[a][1]+image.get_height()/2. + .5 ;
+        z1 = nuage[a][2]+image.get_height()/2.+.5;
 
-        x2 = nuage[b][0]+image.get_width()/2;
-        y2 = nuage[b][1]+image.get_height()/2;
 
-        x3 = nuage[c][0]+image.get_width()/2;
-        y3 = nuage[c][1]+image.get_height()/2;
+        x2 = nuage[b][0]+image.get_width()/2. + .5 ;
+        y2 = nuage[b][1]+image.get_height()/2. + .5 ;
+        z2 = nuage[b][2]+image.get_height()/2.+.5;
 
-        line(x1,y1,x2,y2,image,white);
-        line(x2,y2,x3,y3,image,white);
-        line(x1,y1,x3,y3,image,white);
 
-        setRemplissageTriangleBarycentric(x1,y1,x2,y2,x3,y3,image);
+        x3 = nuage[c][0]+image.get_width()/2. + .5 ;
+        y3 = nuage[c][1]+image.get_height()/2. + .5 ;
+        z3 = nuage[c][2]+image.get_height()/2.+.5;
+
+
+        //line(x1,y1,x2,y2,image,white);
+       // line(x2,y2,x3,y3,image,white);
+       // line(x1,y1,x3,y3,image,white);
+
+        setRemplissageTriangleBarycentric(x1,y1,z1,x2,y2,z2,x3,y3,z3,image,buffer);
 
         //setRemplissageTriangleLineSweep(x1,y1,x2,y2,x3,y3, image);
 	}
@@ -358,13 +381,13 @@ int main(int argc, char** argv) {
     line(200,300,500,400,imageTest,red);
 
 
-    for(int px = 0; px < 500 ; px++){
+    /*for(int px = 0; px < 500 ; px++){
             for(int py = 0;py <500; py++){
-                if(isPointDansTriangle(px,py,60,100,200,300,500,400)){
+                if(isPointDansTriangle(px,py,60,100,0,200,300,0,500,400,0,)){
                     imageTest.set(px,py,white);
                 }
             }
-        }
+        }*/
     //printf("%s",isPointDansTriangle(250,250,60,100,200,300,500,400)?"true":"false");
    // imageTest.set(250,250,white);
     imageTest.flip_vertically();
